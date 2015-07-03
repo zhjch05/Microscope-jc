7ba0eae12cd996ce99cd765a18183b7d57289cc5
Jincheng Zhang <zjc1996@brandeis.edu>
2015-07-03 09:47:27 -0400

    chapter 4-3

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,17 +1,13 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
+});
+
+Template.postItem.helpers({
+  domain: function() {
+    var a = document.createElement('a');
+    a.href = this.url;
+    return a.hostname;
+  }
 });
