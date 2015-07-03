c222254d714102fd081cf558bc26f1a3b44417b1
Jincheng Zhang <zjc1996@brandeis.edu>
2015-07-02 21:02:34 -0400

    back

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,12 +1,17 @@
-postsData = [
+var postsData = [
+  {
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope/'
-    ,
+  },
+  {
     title: 'Meteor',
     url: 'http://meteor.com'
-    ,
+  },
+  {
     title: 'The Meteor Book',
     url: 'http://themeteorbook.com'
+  }
 ];
-Template postsList helpers
+Template.postsList.helpers({
   posts: postsData
+});
